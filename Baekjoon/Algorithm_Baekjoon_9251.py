871dfb13c4a26503475fe5b1fb28b9ba26db4fb2
ngj1014 <ngj1014@naver.com>
2022-01-15 21:09:50 +0900

    DP_2일차
    DP는 점화식과 행렬식을 이용하고, 재귀로도 풀 수 있음을 알자.

--- Baekjoon/Algorithm_Baekjoon_9251.py
+++ Baekjoon/Algorithm_Baekjoon_9251.py
@@ -17,10 +17,10 @@ for i in range(n):
                 break
         break
     dp[i]=result
-
 print(max(dp))
 '''
 
+'''
 x=input()
 y=input()
 
@@ -34,3 +34,26 @@ for i in range(1,len(x)+1):
             dp[i][j]=max(dp[i][j-1],dp[i-1][j])
 
 print(dp[len(x)][len(y)])
+'''
+#재귀 이용하는 방법...
+import sys
+sys.setrecursionlimit(10**7)
+
+def sol(i, j):
+
+    if i < 0 or j < 0:
+        return 0
+
+    if d[i][j] >= 0:
+        return d[i][j]
+
+    if s1[i] == s2[j]:
+        d[i][j] = 1 + sol(i-1, j-1)
+    else:
+        d[i][j] = max(sol(i-1, j), sol(i, j-1))
+
+    return d[i][j]
+
+s1, s2 = input().rstrip(), input().rstrip()
+d = [[-1]*1000 for _ in range(1000)]
+print(sol(len(s1)-1, len(s2)-1))
