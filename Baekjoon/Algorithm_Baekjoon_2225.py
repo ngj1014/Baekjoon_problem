271056205e8f29dd6c5f5118f2063a6cd748635d
ngj1014 <ngj1014@naver.com>
2022-02-11 19:29:28 +0900

    dp_problem
    dp_problem

---
+++ Baekjoon/Algorithm_Baekjoon_2225.py
@@ -0,0 +1,18 @@
+import sys
+
+n,k=map(int, input().split())
+
+dp=[[0]*(201) for _ in range(201)]
+
+#표 한번 그려보자.
+for i in range(n+1):
+    dp[1][i]=1
+    dp[2][i]=i+1
+
+for i in range(2,k+1):
+    dp[i][1]=i
+    for j in range(2,n+1):
+        dp[i][j] = (dp[i][j-1]+dp[i-1][j]) % 1000000000
+
+
+print(dp[k][n])
